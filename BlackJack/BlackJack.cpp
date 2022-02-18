98384d2bedecc1b0e82af8dbd573663696981758
mrjoeyrules <mrjoeyrules@gmail.com>
2022-02-18 16:45:03 +0000

    Added INT validation

--- BlackJack/BlackJack.cpp
+++ BlackJack/BlackJack.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-
+class Validation 
+{
+    int intValidation(string inputMessage)
+    {
+        bool loopControl = true;
+        while (loopControl)
+        {
+            string rawValue;
+            cout << inputMessage << endl;
+            cin >> rawValue;
+            try
+            {
+                int convertedValue;
+                convertedValue = stoi(rawValue);
+                loopControl = false;
+                return convertedValue;
+            }
+            catch (const std::exception&)
+            {
+                cout << "You must enter a whole number, Try Again" << endl;
+            }
+        }
+    }
+};
 
 int main()
 {
